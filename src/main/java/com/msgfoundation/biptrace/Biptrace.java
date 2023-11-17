f28a9ec189d9e5bb2d6bfb3e7dd4982549845704
jhonalexander019 <jhon.roa@unillanos.edu.co>
2023-11-17 14:08:45 -0500

    readme created and parameter -p aggregate

--- src/main/java/com/msgfoundation/biptrace/Biptrace.java
+++ src/main/java/com/msgfoundation/biptrace/Biptrace.java
@@ -2,23 +2,38 @@ package com.msgfoundation.biptrace;
 
 public class Biptrace {
 
-    static String projectPath = "C:\\Users\\danil\\OneDrive\\Documentos\\MsgFoundation\\MSGF-CentralSys";
-
     public static void main(String[] args) {
-        if (args.length > 0 && args[0].equalsIgnoreCase("-f")) {
-            if (args.length > 1) {
-                String outputFileName = args[1];
-                performCustomAction(projectPath, outputFileName);
-            } else {
-                System.err.println("Missing output file name. Usage: java -jar yourjar.jar f <outputFileName>");
+        String projectPath = null; // Inicializado como null
+        String outputFileName = ""; // Por defecto, una cadena vacía
+
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].equalsIgnoreCase("-p") && i + 1 < args.length) {
+                    projectPath = args[i + 1];
+                    i++; // Avanzar al siguiente argumento después de -p
+                } else if (args[i].equalsIgnoreCase("-f")) {
+                    if (i + 1 < args.length) {
+                        outputFileName = args[i + 1];
+                        i++; // Avanzar al siguiente argumento después de -f
+                    } else {
+                        System.err.println("Missing output file name. Usage: java -jar yourjar.jar -p 'projectPath' [-f <outputFileName>]");
+                        return;
+                    }
+                }
             }
         } else {
-            // Llama a la lógica de análisis
-            AnnotationAnalyzer.analyzeAnnotationsInProject(projectPath, "");
+            System.err.println("Please provide project path using -p 'projectPath'.");
+            return;
         }
-    }
 
-    private static void performCustomAction(String projectPath, String outputFileName) {
+        // Si projectPath sigue siendo null, significa que -p no fue proporcionado correctamente
+        if (projectPath == null) {
+            System.err.println("Missing project path. Usage: java -jar yourjar.jar -p <projectPath> [-f <outputFileName>]");
+            return;
+        }
+
+        // Realizar acciones basadas en los valores obtenidos
         AnnotationAnalyzer.analyzeAnnotationsInProject(projectPath, outputFileName);
     }
 }
+
