f5508d79825c86cd5de754b9ada78d92b54752c3
Martin-Perez-Rico <martin.perez@unillanos.edu.co>
2024-01-05 20:09:21 -0500

    implemet method for save data base

---
+++ src/main/java/com/mycompany/trazability/Main/Trazability.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trazability.Main;
+
+import com.mycompany.trazability.DataBase.Connections;
+import com.mycompany.trazability.Extraction.Data;
+
+/**
+ *
+ * @author User
+ */
+public class Trazability {
+
+    public static void main(String[] args) {
+        Data data = new Data("D:\\Documentos\\EPI\\trazability\\src\\main\\java\\com\\mycompany\\trazability\\Json\\MSG-Foundation.json","D:\\Documentos\\EPI\\trazability\\src\\main\\java\\com\\mycompany\\trazability\\Json\\MSGF-Test.json","MSG-Foundation");
+    }
+}
